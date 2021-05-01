d9be6ad00df7780927b5a2b0290c9d5e43912673
Sahil Jain <sahil@princeton.edu>
2021-04-30 20:55:48 -0400

    more support

--- src/cycle_sim.cpp
+++ src/cycle_sim.cpp
@@ -3,9 +3,11 @@
 #include "MemoryStore.h"
 #include "RegisterInfo.h"
 #include "CacheConfig.h"
-#include "DriverFunctions.h"
 #include "EndianHelpers.h"
+#include <math.h>
 #include <vector>
+#include "cache_test.h"
+
 using namespace std;
 
 /* Global Variable Definitions */
@@ -28,6 +30,7 @@ struct Cache {
   void resize(size_t size) {
     entries.resize(size);
   }
+  
   uint32_t tag_bits;
   uint32_t index_bits;
   uint32_t block_bits;
@@ -47,11 +50,13 @@ static RegisterInfo regInfo;
 static uint32_t PC = 0x00000000;
 static uint32_t nPC = WORD_SIZE;
 
-static uint32_t icHits = 0;
-static uint32_t icMisses = 0;
-static uint32_t dcHits = 0;
-static uint32_t dcMisses = 0;
-static uint32_t totalCycles = 0;
+uint32_t icHits = 0;
+uint32_t icMisses = 0;
+uint32_t dcHits = 0;
+uint32_t dcMisses = 0;
+uint32_t totalCycles = 0;
+
+/* End of Global Variable Definitions */
 
 struct IFID {
   uint32_t nPC;
@@ -67,7 +72,8 @@ struct IDEX {
   uint32_t RD;
   uint32_t immed;
   uint32_t A;
-  uint32_t B;
+  uint32_t B; 
+  uint32_t seimmed;
 };
 
 struct EXMEM {
@@ -99,6 +105,8 @@ mem_wb_cpy = MEMWB(0, 0, 0);
 
 /* End of Global Variable Definitions */
 
+/* Start of the Cache Files */
+
 int initSimulator(CacheConfig & icConfig, CacheConfig & dcConfig, MemoryStore *mainMem)
 {
   myMem = mainMem;
@@ -106,134 +114,210 @@ int initSimulator(CacheConfig & icConfig, CacheConfig & dcConfig, MemoryStore *m
   bool dIsDirect = (dcConfig.type == DIRECT_MAPPED) ? true : false;
   int iWays = (icConfig.type == DIRECT_MAPPED) ? 1 : 2;
   int dWays = (dcConfig.type == DIRECT_MAPPED) ? 1 : 2;
+  int iBlockSize = icConfig.blockSize;
+  int dBlockSize = dcConfig.blockSize;
 
   uint32_t index_size = icConfig.cacheSize / (icConfig.blockSize * iWays);
   uint32_t index_bits = log2(index_size);
-  uint32_t block_offset = icConfig.blockSize / WORD_SIZE;
-  uint32_t block_offset_bits = log2(block_offset);
+  uint32_t block_words = iBlockSize / WORD_SIZE;
+  uint32_t block_offset_bits = log2(block_words);
   
-  uint32_t tag_bits = 32 - index_bits - block_offset_bits - 2;
+  uint32_t tag_bits = 16 - index_bits - block_offset_bits - 2;
   
-  CacheEntry iEntries = new CacheEntry[index_size];
-  iCache = Cache(iEntries, tag_bits, index_bits, block_offset_bits, true, icConfig.missLatency, iIsDirect);
-    
-  index_size = dcConfig.cacheSize / (dcConfig.blockSize * dWays);
-  index_bits = log2(index_size);
-  block_offset = dcConfig.blockSize / WORD_SIZE;
-  block_offset_bits = log2(block_offset);
-  tag_bits = 32 - index_bits- block_offset_bits - 2;
+  // initialize iCache
+  iCache.tag_bits = tag_bits;
+  iCache.index_bits = index_bits;
+  iCache.block_bits = block_offset_bits;
+  iCache.isiCache = true;
+  iCache.missLatency = icConfig.missLatency;
+  iCache.isDirect = iIsDirect; 
+  iCache.resize(index_size);
+  for (int i = 0; i < index_size; i++) {
+    iCache.entries[i].resize(block_words);
+  }
 
-  CacheEntry dEntries = new CacheEntry[index_size];
-  dCache = Cache(dEntries, tag_bits, index_bits,block_offset_bits, false, dcConfig.missLatency, dIsDirect);
+  index_size = dcConfig.cacheSize / (dBlockSize * dWays);
+  index_bits = log2(index_size);
+  block_words = dBlockSize / WORD_SIZE;
+  block_offset_bits = log2(block_words);
+  tag_bits = 16 - index_bits - block_offset_bits - 2;
+
+  // initialize dCache
+  dCache.tag_bits = tag_bits;
+  dCache.index_bits = index_bits;
+  dCache.block_bits = block_offset_bits;
+  dCache.isiCache = false;
+  dCache.missLatency = dcConfig.missLatency;
+  dCache.isDirect = dIsDirect; 
+  dCache.resize(index_size);
+  for (int i = 0; i < index_size; i++) {
+    dCache.entries[i].resize(block_words);
+  }
 
   // TODO: initialize iCache and dCache sizes, initialize individual cache entry data sizes 
   // TODO: test w driver
+
   return 0;  
 
 }
-static bool cacheRead(Cache cache, uint32_t memAddress, uint32_t* data) 
-{
-  // first figure out the index
 
-  uint32_t index = memAddress >> 2 >> cache.block_bits;
-  uint32_t bitmask = 1 << cache.index_bits;
+// TODO: static function for block eviction
+
+static void evict_block(bool isICache, uint32_t index) {
+
+  Cache* cache = isICache ? &iCache : &dCache;
+  uint32_t block_size = 1 << cache->block_bits;
+
+  for (int i = 0; i < block_size; i++) {
+    uint32_t first_block_address = cache->entries[index].tag << cache->index_bits;
+    first_block_address |= index;
+    first_block_address <<= cache->block_bits + 2;
+    myMem->setMemValue(first_block_address + 4 * i, cache->entries[index].data[i], WORD_SIZE);
+  }  
+}
+
+// TODO: static function for reading block from memory into cache
+
+static void read_from_mem(bool isICache, uint32_t index) {
+
+  Cache* cache = isICache ? &iCache : &dCache;
+  uint32_t block_size = 1 << cache->block_bits;
+
+  for (int i = 0; i < block_size; i++) {
+    uint32_t first_block_address = cache->entries[index].tag << cache->index_bits;
+    first_block_address |= index;
+    first_block_address <<= cache->block_bits + 2;
+    myMem->getMemValue(first_block_address + 4 * i, cache->entries[index].data[i], WORD_SIZE);
+  }
+}
+
+// prints statistics
+void printStats() {
+  cout << "iCache Hits: " << icHits << endl;
+  cout << "iCache Misses: " << icMisses << endl;
+  cout << "dCache Hits: " << dcHits << endl;
+  cout << "dCache Misses: " << dcMisses << endl;
+}
+
+// handles cache reads and writes
+bool cacheAccess(bool isICache, uint32_t memAddress, uint32_t *data, bool isRead) 
+{
+  Cache* cache = isICache ? &iCache : &dCache;
+
+  // first figure out the index
+  uint32_t index = memAddress >> 2 >> cache->block_bits;
+  uint32_t bitmask = 1 << cache->index_bits;
   bitmask = bitmask - 1;
   index = index & bitmask;
 
-  tag = tag >> (cache.block_bits + cache.index_bits + 2);
-  index *= ((cache.isDirect) ? 1 : 2);
+  uint32_t tag = memAddress >> (cache->block_bits + cache->index_bits + 2);
+  index *= ((cache->isDirect) ? 1 : 2);
 
-  block_offset = memAddress >> 2;
-  bitmask = 1 << cache.block_bits;
+  uint32_t block_offset = memAddress >> 2;
+  bitmask = 1 << cache->block_bits;
   bitmask--;
   block_offset &= bitmask;
 
-  if (cache.isDirect) {
+
+  if (cache->isDirect) {
     // handles read hit
-    if (cache.entries[index].valid && cache.entries[index].tag == tag) {
-      *data = cache.entries[index].data[block_offset];
+    if (cache->entries[index].isValid && cache->entries[index].tag == tag) {
+      if (isICache) icHits++;
+      else dcHits++;      
+
+      if (isRead) {
+        *data = cache->entries[index].data[block_offset];
+      } else {
+        cache->entries[index].data[block_offset] = *data;
+      }
       return true;
     } else {
       // if valid, evict existing block and write to memory
-      if (cache.entries[index].valid) {
-        // copy contents of cache entry to memory
-        for (int i = 0; i < block_size; i++) {
-          first_block_address = cache.entries[index].tag << cache.index_bits;
-          first_block_address |= index;
-          first_block_address <<= block_bits + 2;
-          myMem->setMemValue(first_block_address + 4 * i, cache.entries[index].data[block_offset + i], WORD_SIZE);
-        }
+      if (isICache) icMisses++;
+      else dcMisses++;
+      if (cache->entries[index].isValid) {
+        evict_block(isICache, index);
       } 
 
       // read from memAddress to cache
-      for (int i = 0; i < block_size; i++) {
-        first_block_address = tag << cache.index_bits;
-        first_block_address |= index;
-        first_block_address <<= block_bits + 2;
-        myMem->getMemValue(first_block_address + 4 * i, cache.entries[index].data[block_offset + i], WORD_SIZE);
-      }
+      read_from_mem(true, index);
       
       // read from cache into data
-      cache.entries[index].isValid = true;
-      cache.entries[index].tag = tag;
-      *data = cache.entries[index].data[block_offset];
+      cache->entries[index].isValid = true;
+      cache->entries[index].tag = tag;
+      if (isRead) {
+        *data = cache->entries[index].data[block_offset];
+      } else {
+        cache->entries[index].data[block_offset] = *data;
+      }
       return false; 
     }
   } else {
     // cache hit with first block in set
-    if (cache.entries[index].valid && cache.entries[index].tag == tag) {
-      *data = cache.entries[index].data[block_offset];
-      cache.entries[index].isMRU = true;
-      cache.entries[index + 1].isMRU = false;
+    if (cache->entries[index].isValid && cache->entries[index].tag == tag) {
+      if (isICache) icHits++;
+      else dcHits++;      
+      if (isRead) {
+        *data = cache->entries[index].data[block_offset];
+      } else {
+        cache->entries[index].data[block_offset] = *data;
+      } 
+
+      cache->entries[index].isMRU = true;
+      cache->entries[index + 1].isMRU = false;
+ 
       return true;
     // cache hit with second block in set
-    } else if (cache.entries[index + 1].valid && cache.entries[index + 1].tag == tag) {
-      *data = cache.entries[index + 1].data[block_offset];
-      cache.entries[index].isMRU = false;
-      cache.entries[index + 1].isMRU = true;
+    } else if (cache->entries[index + 1].isValid && cache->entries[index + 1].tag == tag) {
+      if (isICache) icHits++;
+      else dcHits++;      
+      if (isRead) {
+        *data = cache->entries[index].data[block_offset];
+      } else {
+        cache->entries[index].data[block_offset] = *data;
+      } 
+
+      cache->entries[index].isMRU = false;
+      cache->entries[index + 1].isMRU = true;
       return true;
       // cache miss
     } else {
+      if (isICache) icMisses++;
+      else dcMisses++;
       // find index of block in the set that is LRU to evict
-      firstIsLRU = (cache.entries[index].isMRU) ? 0 : 1;
+      int firstIsLRU = (cache->entries[index].isMRU) ? 0 : 1;
 
       // write every other entry in cache.entries to memory
-      for (int i = 0; i < block_size; i += 2) {
-        first_block_address = cache.entries[index + firstIsLRU].tag << cache.index_bits;
-        first_block_address |= index + firstIsLRU;
-        first_block_address <<= block_bits + 2;
-        myMem->setMemValue(first_block_address + 4 * i, cache.entries[index + firstIsLRU].data[block_offset + i], WORD_SIZE);
+      if (cache->entries[index + firstIsLRU].isValid) {
+        evict_block(isICache, index + firstIsLRU);
       }
-
       // read from memAddress to cache
-      for (int i = 0; i < block_size; i++) {
-        first_block_address = tag << cache.index_bits;
-        first_block_address |= index + firstIsLRU;
-        first_block_address <<= block_bits + 2;
-        myMem->getMemValue(first_block_address + 4 * i, cache.entries[index + firstIsLRU].data[block_offset + i], WORD_SIZE);
-      }
+      read_from_mem(true, index + firstIsLRU);
 
       // read from cache into data
-      cache.entries[index + firstIsLRU].isValid = true;
-      cache.entries[index + firstIsLRU].tag = tag;
-      *data = cache.entries[index + firstIsLRU].data[block_offset];
+      cache->entries[index + firstIsLRU].isValid = true;
+      cache->entries[index + firstIsLRU].tag = tag;
+      *data = cache->entries[index + firstIsLRU].data[block_offset];
 
       // set MRU bit for both blocks in the set
-      cache.entries[index + firstIsLRU].isMRU = true;
-      cache.entries[index + !firstIsLRU].isMRU = false;
+      cache->entries[index + firstIsLRU].isMRU = true;
+      cache->entries[index + !firstIsLRU].isMRU = false;
 
       return false;
     }
   }
-
 } 
+/* End of Cache Files */ 
 
-static bool cacheWrite(Cache cache, uint32_t memAddress, uint32_t* data) {
-  
+ 
+void advance_pc(uint32_t offset)
+{
+    PC  =  nPC;
+   nPC  += offset;
 }
 
 
-
+// run cycles function
 int runCycles(uint32_t cycles) {
 
   uint32_t cyclesElapsed = 0;
@@ -243,15 +327,12 @@ int runCycles(uint32_t cycles) {
     /* IF section  */
     uint32_t instruction = 0;
 
-    bool hit = cacheRead(iCache, PC, &instruction);
+    bool hit = cacheAccess(true, PC, &instruction, true);
     if (!hit) {
-      cyclesElapsed += iCache.missLatency;
-      instruction = myMem->getMemValue(PC, da, WORD_SIZE);
-      cacheWrite(iCache, PC, instruction);
+      // TODO: Add stalling logic here.
     }
 
-    PC = PC + 4;    
-    if_id.nPC = PC;
+    if_id.nPC = PC + 4;
     if_id.ir = instruction;
 
     /* ID section  */
@@ -262,24 +343,71 @@ int runCycles(uint32_t cycles) {
     id_ex.opcode = instruction >> 26;
     id_ex.RS = instruction << 6 >> 27;
     id_ex.RT = instruction << 11 >> 27;
-    id_ex.RD = instructon << 16 >> 27;
+    id_ex.RD = instruction << 16 >> 27;
     id_ex.func_code = instruction && (63); 
     id_ex.immed = instruction << 16 >> 16;
     id_ex.A = reg[id_ex.RS];
     id_ex.B = reg[id_ex.RT];
-    id_ex.shamt = instructon << 21 >> 27;
+    id_ex.shamt = instruction << 21 >> 27;
+    id_ex.seimmed = (id_ex.immed >> 15 == 0) ? (uint32_t)id_ex.immed : ((uint32_t)id_ex.immed | 0xffff0000);
 
-    /* 
-
-    TODO: Determine if it's a branch. Enter Code Here 
-
-    */
+    uint32_t mostSig_ex = imm >> 15; // most significant bit in immediate
+    uint32_t imm_ex = 0;
+    /* Determine if it's a branch. Enter Code Here */
+    switch (id_ex.opcode) {
+      // beq
+      case (0x4):
+        if (id_ex.A == id_ex.B) {
+            advance_pc(id_ex.seimmed << 2);
+        }
+        break;
+      // bneq
+      case (0x5):
+        if (id_ex.A != id_ex.B) {
+            advance_pc(id_ex.seimmed << 2);
+        }
+        break;
+      // jump
+      case (0x2):
+        PC = nPC; 
+        nPC = (PC & 0xf0000000) | (instruction << 6) >> 4;
+        break;
+      // jump and link
+      case (0x3):
+        id_ex.RD = 31;
+        id_ex.A = nPC + 4;
+        PC = nPC; 
+        nPC = (PC & 0xf0000000) | (instruction << 6) >> 4;
+        break;
+      // blez
+      case (0x6):
+        if (id_ex.A <= id_ex.B) {
+            advance_pc(id_ex.seimmed << 2);
+        }
+        break;
+      // bgtz
+      case (0x7):
+        if (id_ex.A > id_ex.B) {
+            advance_pc(id_ex.seimmed << 2);
+        }
+        break;
+      // jump register
+      case (0x0):
+        if (id_ex.func_code == 0x8) {
+          PC = nPC; 
+          nPC = id_ex.A;
+        }
+        break;
+      default:
+        branchTaken = false;
+        break;
+    }
 
     id_ex.nPC = if_id_cpy.nPC + 4;
 
-    /* Enter of ID Section */
+    /* End of ID Section */
 
-    /* EX Section */ 
+    /* Start of EX Section */ 
     int opCode = id_ex_cpy.opcode;
     // initialize variables, rs, rt, rd, imm, mostSig
     uint32_t rs = id_ex_copy.RS; // operand
@@ -296,6 +424,11 @@ int runCycles(uint32_t cycles) {
     ex_mem.B = id_ex_copy.B;
     if (opCode == 0) {
       ex_mem.RD = rd;
+    }
+    else if (opCode == 0x3) {
+      ex_mem.RD = rd;
+      ex_mem.B = id_ex_copy.A;
+      regWrite = true;
     }
     else {
       ex_mem.RD = rt;
@@ -465,6 +598,7 @@ int runCycles(uint32_t cycles) {
     }
     case 0x24:
     {
+      regWrite = true;
         // load byte unsigned
         // sign extend the imm variable
         if (mostSig == 1) {
@@ -478,6 +612,7 @@ int runCycles(uint32_t cycles) {
     }
     case 0x25:
     {
+      regWrite = true;
         // load half word unsigned
         // sign extend the imm variable
         if (mostSig == 1) {
@@ -491,6 +626,7 @@ int runCycles(uint32_t cycles) {
     }
     case 0xf:
     {
+      regWrite = true;
         // load upper immediate
         ex_mem.ALUOut = (imm << 16);
         memRead = true;
@@ -498,6 +634,7 @@ int runCycles(uint32_t cycles) {
     }
     case 0x23:
     {
+      regWrite = true;
         // load word
         uint32_t res = 0;
         if (mostSig == 1) {
@@ -511,12 +648,14 @@ int runCycles(uint32_t cycles) {
     }
     case 0xd:
     {
+      regWrite = true;
         // or immediate
         ex_mem.ALUOut = A | imm;
         break;
     }
     case 0x28:
     {
+      regWrite = false;
         // store byte
         if (mostSig == 1) {
             imm = imm | 0xffff0000;
@@ -527,11 +666,13 @@ int runCycles(uint32_t cycles) {
         value = B << 24;
         value = value >> 24; 
         ex_mem.ALUOut = value;
+        ex_mem.B = ex_mem.B && (0x000000ff);
         memWrite = true;
         break;
     }
     case 0x29:
     {
+      regWrite = false;
         memWrite = true;
         // store halfword 
         if (mostSig == 1) {
@@ -542,21 +683,23 @@ int runCycles(uint32_t cycles) {
         uint32_t value = 0;
         value = B << 16;
         value = value >> 16; 
+        ex_mem.B = ex_mem.B && (0x0000ffff);
         ex_mem.ALUOut = value;
         break;
     }
     case 0x2b:
     {
-        memWrite = true;
-        // store word
-        if (mostSig == 1) {
-            imm = imm | 0xffff0000;
-        }
-        uint32_t location = 0;
-        location = A + imm;
-        uint32_t value = B;
-        ex_mem.ALUOut = value;
-        break;
+      regWrite = false;
+      memWrite = true;
+      // store word
+      if (mostSig == 1) {
+          imm = imm | 0xffff0000;
+      }
+      uint32_t location = 0;
+      location = A + imm;
+      uint32_t value = B;
+      ex_mem.ALUOut = value;
+      break;
     }
     default:
     {
@@ -567,8 +710,6 @@ int runCycles(uint32_t cycles) {
         exit(127);
     }
   }
-
-  regWrite = true;
   ex_mem.memRead = memRead;
   ex_mem.memWrite = memWrite;
   ex_mem.regWrite = regWrite;
@@ -578,32 +719,30 @@ int runCycles(uint32_t cycles) {
   /* Begin Mem Section */
   mem_wb.RD = ex_mem_cpy.RD;
   mem_wb.ALUOut = ex_mem_cpy.ALUOut;
-
+  uint32_t storeData = 0;
   bool memRead_mem = ex_mem_cpy.memRead;
   bool memWrite_mem = ex_mem_cpy.memWrite;
 
   if (memRead_mem) {
-    /* TODO: read the dcache 
-
-    mem_wb.memData = CacheRead()
-
-    */
+    bool hit = cacheAccess(false, ex_mem_cpy.ALUOut, &storeData, true)
+    mem_wb.memData = storeData;
+    if (!hit) {
+      /* TODO: Work on Cache Latency / Stall Here As Well */
+    }
   }
   if (memWrite) {
-    /* 
-
-    TODO: write to the dcache
-
-    CacheWrite(ex_mem_cpy.ALUOut, ex_mem_cpy.B)
-
-    */ 
+    bool hit = cacheAccess(false, ex_mem_cpy.ALUOut, ex_mem_cpy.B, false)
+    if (!hit) {
+      /* TODO: Work on Cache Latency / Stall Here As Well */
+    }
   }
 
   /* End of Mem Section */ 
 
   /* Start of WB Section */
-
-  reg[mem_wb_cpy.RD] = mem_wb_cpy.ALUOut;
+  if (regWrite) {
+    reg[mem_wb_cpy.RD] = mem_wb_cpy.ALUOut;
+  }
 
   /* End of WB Section */
 
@@ -620,6 +759,7 @@ int runCycles(uint32_t cycles) {
   id_ex_cpy.immed = id_ex.immed;
   id_ex_cpy.A = id_ex.A;
   id_ex_cpy.B = id_ex.B;
+  id_ex_cpy.seimmed = id_ex.seimmed;
 
   ex_mem_cpy.BrTgt = ex_mem.BrTgt;
   ex_mem_cpy.Zero = ex_mem.Zero;
