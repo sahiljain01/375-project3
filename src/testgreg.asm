237a08d1ce552168e366589456b9ad9c4f645af8
Gregory Smith <gpsmith@princeton.edu>
2021-05-08 18:31:30 -0400

    m

--- src/testgreg.asm
+++ src/testgreg.asm
@@ -1,13 +1,12 @@
 # Overflow test case for add
 .set noreorder
 addi $s0, $zero, 0x01
-addi $s1, $zero, 0x7fffffff
 nop
 nop
-add $s2, $s0, $s1
+addi $s1, $s0, 0x7fff
 nop
 nop
-sw $s2, next+4
+sw $s1, next+4
 next:
 .word 0xfeedfeed
 .word 0x11111111
