9c917dae26c573fe036f0ed7065b1da20f1ff7c7
Gregory Smith <gpsmith@princeton.edu>
2021-05-08 20:04:27 -0400

    m

--- src/testgreg.asm
+++ src/testgreg.asm
@@ -1,9 +1,9 @@
 # Overflow test case for invalid instr
-.set noreorder
-lw $t0, 0x14($zero);
-nop
-nop
-addi $s1, $t0, 0x1
+    set noreorder
+    lw $t0, next+4
+    nop
+    nop
+    addi $s1, $t0, 0x1
 next:
-.word 0xfeedfeed
-.word 0x7fffffff
+    .word 0xfeedfeed
+    .word 0x7fffffff
