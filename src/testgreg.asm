0680221e834d720c111cda063d9c7679c3ecc175
Gregory Smith <gpsmith@princeton.edu>
2021-05-09 11:00:23 -0400

    m

--- src/testgreg.asm
+++ src/testgreg.asm
@@ -3,10 +3,11 @@
     lw $t1, next+4
     nop
     nop
-    bne $t1, $zero, 0x16
+    bne $t1, $zero, afterbranch
     nop
 next:
     .word 0xfeedfeed
     .word 0x00000016
+afterbranch:
     addi $t2, $zero, 0x99
     .word 0xfeedfeed
